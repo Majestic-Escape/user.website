7b86caeaa9cbb5e972798a475a93f77e806332f2
aditya <adityaparuleker.cr@gmail.com>
2025-12-30 16:04:25 +0530

    Mobile icon

--- src/components/nav-tab-layout-mobile.jsx
+++ src/components/nav-tab-layout-mobile.jsx
@@ -5,18 +5,18 @@ import { usePathname } from "next/navigation";
 import { Tabs, MobileTabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const tabs = [
-  { name: "Stays", id: "1", href: "/stays", icon: "/images/mobile/house.png" },
+  { name: "Stays", id: "1", href: "/stays", icon: "/images/mobile/house1.png" },
   {
     name: "Experiences",
     id: "2",
     href: "/experiences",
-    icon: "/images/mobile/experience.png",
+    icon: "/images/mobile/experience1.png",
   },
   {
     name: "Services",
     id: "3",
     href: "/services",
-    icon: "/images/mobile/service.png",
+    icon: "/images/mobile/service1.png",
   },
 ];
 
@@ -33,21 +33,27 @@ export default function MobileNavTabLayout({ hideTabs }) {
             className={tab.id == 3 ? null : "border-r-2"}
             asChild
           >
-            <Link
-              href={tab.href}
-              className="flex flex-col items-center  justify-center w-full pt-2"
-            >
-              <img src={tab.icon} height="30" width="30" />
-              {/* {tab.id == 1 ? (
+            <>
+              <Link
+                href={tab.href}
+                className="flex flex-col items-center  justify-center w-full pt-2 "
+              >
+                {tab.id == 2 ? (
+                  <img src={tab.icon} height="18" width="18" />
+                ) : (
+                  <img src={tab.icon} height="30" width="30" />
+                )}
+                {/* {tab.id == 1 ? (
                 <img src={tab.icon} height="30" width="30" />
               ) : null}
               {tab.id == 1 ? null : (
                 <span className="text-2xl leading-none">{tab.icon}</span>
               )} */}
-              <span className="text-sm font-medium text-absoluteDark">
-                {tab.name}
-              </span>
-            </Link>
+                <span className="text-sm font-medium text-absoluteDark">
+                  {tab.name}
+                </span>
+              </Link>
+            </>
           </TabsTrigger>
         ))}
       </MobileTabsList>
