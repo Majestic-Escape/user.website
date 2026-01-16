d25d697e720fd82d3847d352dec899efb444d009
aditya <adityaparuleker.cr@gmail.com>
2026-01-16 17:37:29 +0530

    Formatting change

--- src/app/host/dashboard/kyc/components/step-indicator.jsx
+++ src/app/host/dashboard/kyc/components/step-indicator.jsx
@@ -1,17 +1,14 @@
-
-  export function StepIndicator({ currentStep, totalSteps }) {
-    return (
-      <div className="hidden md:flex items-center space-x-1">
-        {Array.from({ length: totalSteps }).map((_, index) => (
-          <div
-            key={index}
-            className={`h-1 w-4 md:w-8 rounded-full ${
-              index <= currentStep ? 'bg-brightGreen' : 'bg-gray-300'
-            }`}
-          />
-        ))}
-      </div>
-    )
-  }
-  
-  
+export function StepIndicator({ currentStep, totalSteps }) {
+  return (
+    <div className="hidden md:flex items-center space-x-1">
+      {Array.from({ length: totalSteps }).map((_, index) => (
+        <div
+          key={index}
+          className={`h-1 w-4 md:w-8 rounded-full ${
+            index <= currentStep ? "bg-brightGreen" : "bg-gray-300"
+          }`}
+        />
+      ))}
+    </div>
+  );
+}
