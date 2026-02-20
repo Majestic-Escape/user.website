c8911f781da61192205067f9c546457ad93036c6
aditya <adityaparuleker.cr@gmail.com>
2026-02-20 18:57:51 +0530

    Manage booking and messaging children bug fix

--- src/app/(landing)/manage-bookings/page.jsx
+++ src/app/(landing)/manage-bookings/page.jsx
@@ -1,12 +1,22 @@
+import React, { Suspense } from "react";
+import BookingSummaryPage from "./booking-summary-page";
+import BookingList from "./BookingList";
 export const metadata = {
   robots: {
     index: false,
     follow: false,
   },
 };
-
-import BookingList from "./BookingList";
-
-export default function HostLayout({ children }) {
-  return <BookingList>{children}</BookingList>;
+export default function HostLayout() {
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-current"></div>
+        </div>
+      }
+    >
+      <BookingList />
+    </Suspense>
+  );
 }
--- src/app/(landing)/messages/page.jsx
+++ src/app/(landing)/messages/page.jsx
@@ -1,12 +1,22 @@
+import React, { Suspense } from "react";
+import Messaging from "./Messaging";
+
 export const metadata = {
   robots: {
     index: false,
     follow: false,
   },
 };
-
-import Messaging from "./Messaging";
-
-export default function MessageLayout({ children }) {
-  return <Messaging>{children}</Messaging>;
+export default function MessageLayout() {
+  return (
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="h-20 w-20 animate-spin rounded-full border-b-2 border-current"></div>
+        </div>
+      }
+    >
+      <Messaging />
+    </Suspense>
+  );
 }
