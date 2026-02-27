9ad081d1ec3b2b2c7ee5e57fe8907263cd4d9717
aditya <adityaparuleker.cr@gmail.com>
2026-02-27 10:42:25 +0530

    Priority 1 SEO fix

--- src/app/(landing)/hosting/page.jsx
+++ src/app/(landing)/hosting/page.jsx
@@ -6,9 +6,15 @@ import Newsletter from "@/components/newsletter";
 import Faq from "@/components/host/faq";
 import Blogs from "@/components/blogs";
 import { useAuth } from "@/contexts/AuthContext";
+import { useEffect, useState } from "react";
 export default function Component() {
-  const userId = localStorage.getItem("userId");
+  // const userId = localStorage.getItem("userId");
+  const [userId, setUserId] = useState(null);
 
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    setUserId(storedUserId);
+  }, []);
   return (
     <div>
       <Hero user={userId} />
