4a4a331ac33f522239ddbafd9245f80a34832807
aditya <adityaparuleker.cr@gmail.com>
2026-03-05 10:08:23 +0530

    Redirect blogs

--- next.config.ts
+++ next.config.ts
@@ -41,7 +41,6 @@ const nextConfig: NextConfig = {
   },
   async redirects() {
     return [
-      // 1. Redirect www to non-www (HTTPS version)
       {
         source: "/:path*",
         has: [
@@ -51,26 +50,33 @@ const nextConfig: NextConfig = {
           },
         ],
         destination: "https://majesticescape.in/:path*",
-        permanent: true, // 301 redirect
+        permanent: true,
       },
 
-      // 2. HTTP to HTTPS redirect (if not handled automatically)
-      // This catches both majesticescape.in and www.majesticescape.in on HTTP
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "header",
-            key: "x-forwarded-proto",
-            value: "http",
-          },
-        ],
-        destination: "https://majesticescape.in/:path*",
+        source: "/blog/1",
+        destination: "/blog/top-5-reasons-why-homestays-are-better-than-hotels",
+        permanent: true,
+      },
+      {
+        source: "/blog/2",
+        destination:
+          "/blog/how-to-plan-a-budget-trip-without-compromising-comfort",
+        permanent: true,
+      },
+      {
+        source: "/blog/3",
+        destination: "/blog/top-5-reasons-why-homestays-are-better-than-hotels",
+        permanent: true,
+      },
+      {
+        source: "/blog/4",
+        destination:
+          "/blog/spice-of-life-a-culinary-journey-through-goan-cuisine",
         permanent: true,
       },
     ];
   },
-
   /* config options here */
 };
 
