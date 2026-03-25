077336ccb989735f902e2116b010169f1d85d705
aditya <adityaparuleker.cr@gmail.com>
2026-03-25 15:38:07 +0530

    Success message display increased to 5 seconds

--- src/components/experience-block.tsx
+++ src/components/experience-block.tsx
@@ -282,6 +282,9 @@ function BookingModal({ isOpen, source, onClose, data }: BookingModalProps) {
       // console.log(json);
       toast.success(
         "Thank you for requesting your itinerary! Our travel experts are crafting the perfect experience for you. Magic is on its way!",
+        {
+          duration: 5000, // 5 seconds (in milliseconds)
+        },
       );
     } catch (error) {
       console.error("Submission error:", error);
